ce2289afc0aa47a01e928b8917bab4609a7f2260
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2023-02-09 20:57:54 -0300

    when create user sucessfully redirect to /users-list, when it fails show the error

--- src/create-user/create-user-page.tsx
+++ src/create-user/create-user-page.tsx
@@ -1,23 +1,23 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import CreateUserForm from './create-user-form';
 import useCreateUser from './use-create-user';
 
 function CreateUserPage() {
-  const [validataAndCreateUser, { user }] = useCreateUser();
+  const [validataAndCreateUser, { user, error }] = useCreateUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate('/users-list');
+    }
+  }, [user]);
 
   return (
     <div>
       <h1>Criar usuário</h1>
       <CreateUserForm validateAndCreateUser={validataAndCreateUser} />
-      {user && (
-        <div>
-          <h1>Usuário criado</h1>
-          <p>Nome: {user.name}</p>
-          <p>E-mail: {user.email}</p>
-          <p>Telefone: {user.phone}</p>
-          <p>Data de nascimento: {user.birthDate}</p>
-          <p>Função: {user.role}</p>
-        </div>
-      )}
+      {error && <p>Error: {error.message}</p>}
     </div>
   );
 }
